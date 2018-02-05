c2dcb5e091becfbdbcd17d4a9a8c0d5e7bd05e6c
Cam <cam.marshall940@yahoo.com>
2018-02-04 20:49:49 -0600

    Added analytics

--- public/src/app/client/landing/landing.component.ts
+++ public/src/app/client/landing/landing.component.ts
@@ -1,6 +1,10 @@
 import { VenderService } from './../../services/vender.service';
 import { Component, OnInit } from '@angular/core';
 import { Vender } from '../../models/vender';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
+import { VenueService } from '../../services/venue.service';
+import { Venue } from '../../models/venue';
 
 @Component({
   selector: 'app-landing',
@@ -9,13 +13,27 @@ import { Vender } from '../../models/vender';
 })
 export class LandingComponent implements OnInit {
   venders: Array<Vender>;
+  categories: Array<Category>
+  venues: Array<Venue>
   
   constructor(
     private _venderService: VenderService,
+    private _categoryService: CategoryService,
+    private _venueService: VenueService,
   ) { }
 
   ngOnInit() {
+    this.getCategories();
+    this.getVenues();
     this.getVenders();
+  }
+
+  getCategories(): void {
+    this._categoryService.getCategories((categories) => this.categories = categories.slice(4))
+  }
+
+  getVenues(): void {
+    this._venueService.getVenues((venues) => this.venues = venues)
   }
 
   getVenders(): void {
